c1bd65d0e779d5c4eeb8581eef2f94050879889d
Aaron <ajbird4@gmail.com>
2018-01-21 11:40:50 +0000

    adding failing test for patch with a hasMany relationship

---
+++ tests/TestCase/Integration/JsonApi/DefaultEditActionIntegrationTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace CrudJsonApi\Test\TestCase\Integration\JsonApi;
+
+use CrudJsonApi\Test\TestCase\Integration\JsonApiBaseTestCase;
+
+class DefaultEditActionIntegrationTest extends JsonApiBaseTestCase
+{
+    /**
+     * Make sure successful POST requests return HTTP Status Code 201.
+     *
+     * @link http://jsonapi.org/format/#crud-creating-responses-201
+     * @link https://github.com/FriendsOfCake/crud/issues/496
+     * @return void
+     */
+    public function testSuccessfulPatchReturnsStatusCode201()
+    {
+        $postData = [
+            'data' => [
+                'type' => 'countries',
+                'id' => '1',
+                'attributes' => [
+                    'code' => 'NZ',
+                    'name' => 'New Zealand',
+                    'currency_id' => 1,
+                    'national_capital_id' => 3
+                ]
+            ]
+        ];
+
+        $this->configRequest([
+            'headers' => [
+                'Accept' => 'application/vnd.api+json',
+                'Content-Type' => 'application/vnd.api+json'
+            ],
+            'input' => json_encode($postData)
+        ]);
+
+        $this->patch('/countries/1');
+
+        $this->assertResponseOk();
+        $this->_assertJsonApiResponseHeaders();
+        $this->assertResponseCode(201);
+        $this->assertResponseNotEmpty();
+    }
+}
