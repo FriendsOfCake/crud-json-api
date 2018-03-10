2cd137ff7b0b0629644ddc098f690db860e10cc4
Rachman Chavik <rchavik@xintesa.com>
2018-03-10 19:31:45 +0700

    Add SortingIntegrationTest

---
+++ tests/TestCase/Integration/JsonApi/SortingIntegrationTest.php
@@ -0,0 +1,58 @@
+<?php
+namespace CrudJsonApi\Test\TestCase\Integration\JsonApi;
+
+use Cake\Event\Event;
+use Cake\Event\EventManager;
+use CrudJsonApi\Test\TestCase\Integration\JsonApiBaseTestCase;
+
+class SortQueryIntegrationTest extends JsonApiBaseTestCase
+{
+    /**
+     * @return array
+     */
+    public function viewProvider()
+    {
+        return [
+            'unsorted' => [
+                '/currencies',
+                'get_currencies_no_sort.json',
+            ],
+            'sorted' => [
+                '/currencies?sort=code',
+                'get_currencies_no_sort.json',
+            ],
+            'sorted descending' => [
+                '/currencies?sort=-code',
+                'get_currencies_sort_by_code_desc.json',
+            ],
+            'unsorted with include' => [
+                '/currencies?include=countries',
+                'get_currencies_and_countries_no_sort.json',
+            ],
+            'sorted with include' => [
+                '/currencies?include=countries&sort=countries.code',
+                'get_currencies_and_countries_sort_by_code.json',
+            ],
+            'sorted desc with include' => [
+                '/currencies?include=countries&sort=-countries.code',
+                'get_currencies_and_countries_sort_by_code_desc.json',
+            ],
+        ];
+    }
+
+    /**
+     * @param string $url The endpoint to hit
+     * @param string $expectedFile The file to find the expected result in
+     * @return void
+     * @dataProvider viewProvider
+     */
+    public function testView($url, $expectedFile)
+    {
+        $this->get($url);
+
+        $this->assertResponseSuccess();
+        $this->_assertJsonApiResponseHeaders();
+        $this->assertResponseEquals($this->_getExpected($expectedFile));
+    }
+
+}
