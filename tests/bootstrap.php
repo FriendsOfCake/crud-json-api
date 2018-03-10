41d2737b1f40a0cc40b3eea4a7a6dc077d7f16f4
bravo-kernel <bravo-kernel@users.noreply.github.com>
2018-03-10 10:27:21 +0000

    Load Search

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -73,6 +73,7 @@ Cake\Core\Configure::write('Session', [
 
 Cake\Core\Plugin::load('Crud', ['path' => ROOT . DS, 'autoload' => true]);
 Cake\Core\Plugin::load('CrudJsonApi', ['path' => ROOT . DS, 'autoload' => true]);
+Cake\Core\Plugin::load('Search', ['path' => ROOT . DS, 'autoload' => true]);
 
 Cake\Routing\DispatcherFactory::add('Routing');
 Cake\Routing\DispatcherFactory::add('ControllerFactory');
