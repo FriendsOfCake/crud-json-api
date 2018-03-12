0796d0f18ce364cf08f8449a67413deebccdb256
Oskar Laska <oskar.laska@republic-m.com>
2018-03-12 08:44:07 +0100

    Add integration tests for self-references assocs.

---
+++ tests/TestCase/Integration/JsonApi/SelfReferencedAssocIntergraionTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace CrudJsonApi\Test\TestCase\Integration\JsonApi;
+
+use CrudJsonApi\Test\TestCase\Integration\JsonApiBaseTestCase;
+
+class SelfReferencedAssocIntegrationTest extends JsonApiBaseTestCase
+{
+    /**
+     * @return array
+     */
+    public function viewProvider()
+    {
+        return [
+            'get supercountry with subcountries' => [
+                '/countries/3?include=subcountries',
+                'get_supercountry_with_subcountries.json'
+            ],
+            'get subcountry with supercountry' => [
+                '/countries/4?include=supercountries',
+                'get_subcountry_with_supercountry.json'
+            ]
+        ];
+    }
+
+    /**
+     * @param string $url The endpoint to hit
+     * @param string $expectedFile The file to find the expected result in
+     * @return void
+     * @dataProvider viewProvider
+     */
+    public function testView($url, $expectedFile)
+    {
+        $this->get($url);
+
+        $this->assertResponseSuccess();
+        $this->_assertJsonApiResponseHeaders();
+        $this->assertResponseEquals($this->_getExpected($expectedFile));
+    }
+
+}
