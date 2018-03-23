91f88ba7a23e4a20219112ea2c117552721d7e3b
bravo-kernel <bravo-kernel@users.noreply.github.com>
2018-03-23 18:59:33 +0000

    Fix incorrect dummy_counter (should be identical to id)

--- tests/Fixture/CountriesFixture.php
+++ tests/Fixture/CountriesFixture.php
@@ -21,6 +21,6 @@ class CountriesFixture extends TestFixture
         ['code' => 'NL', 'name' => 'The Netherlands', 'dummy_counter' => 11111, 'currency_id' => 1, 'national_capital_id' => 1],
         ['code' => 'BE', 'name' => 'Belgium', 'dummy_counter' => 22222, 'currency_id' => 1, 'national_capital_id' => 2],
         ['code' => 'IT', 'name' => 'Italy', 'dummy_counter' => 33333, 'currency_id' => 1, 'national_capital_id' => 4],
-        ['code' => 'VT', 'name' => 'Vatican', 'dummy_counter' => 33333, 'currency_id' => 1, 'national_capital_id' => 5, 'supercountry_id' => 3],
+        ['code' => 'VT', 'name' => 'Vatican', 'dummy_counter' => 44444, 'currency_id' => 1, 'national_capital_id' => 5, 'supercountry_id' => 3],
     ];
 }
