81e67d40d48426c50d7283384a6ee9679e4ab9a8
bravo-kernel <bravo-kernel@users.noreply.github.com>
2018-05-18 17:59:08 +0000

    Add PatchRequestIntegrationTest

--- tests/TestCase/Integration/JsonApi/PatchRequestIntegrationTest.php
+++ tests/TestCase/Integration/JsonApi/PatchRequestIntegrationTest.php
@@ -5,41 +5,84 @@ use CrudJsonApi\Test\TestCase\Integration\JsonApiBaseTestCase;
 
 class PatchRequestIntegrationTest extends JsonApiBaseTestCase
 {
+
     /**
-     * Make sure successful PATCH requests return HTTP Status Code 200.
+     * PhpUnit Data Provider for testing (only) successful PATCH requests.
      *
-     * @link http://jsonapi.org/format/#crud-updating-responses-200
-     * @link https://github.com/FriendsOfCake/crud-json-api/issues/23
-     * @return void
+     * Each array-entry is executed against the `testPatch()` method in this Integration test which
+     * will automatically check for the correct (jsonapi) response headers, the expected HTTP Response
+     * Code 200 and the expected (jsonapi) response body.
+     *
+     * @return array
      */
-    public function testSuccessfulPatchReturnsStatusCode200()
+    public function patchProvider()
     {
-        $postData = [
-            'data' => [
-                'type' => 'countries',
-                'id' => '1',
-                'attributes' => [
-                    'code' => 'NZ',
-                    'name' => 'New Zealand',
-                    'currency_id' => 10,
-                    'national_capital_id' => 3
-                ]
-            ]
+        return [
+            #
+            # Test updating a single-word resource.
+            #
+            # By leaving out the `dummy-counter` attribute we also assert the following JSON API criteria:
+            # "If a request does not include all of the attributes for a resource, the server MUST
+            #  interpret the missing attributes as if they were included with their current values.
+            #  The server MUST NOT interpret missing attributes as null values."
+            #
+            'patch-single-word-resource-only' => [
+                '/countries/1',
+                [
+                    'data' => [
+                        'type' => 'countries',
+                        'id' => '1',
+                        'attributes' => [
+                            'code' => 'NL',
+                            'name' => 'Patched Netherlands'
+                        ]
+                    ]
+                ],
+                'PatchRequest/patched-single-word-resource.json',
+            ],
+
+            #
+            # Test updating a multi-word resource.
+            #
+            'patch-multi-word-resource' => [
+                '/national-capitals/1',
+                [
+                    'data' => [
+                        'type' => 'national-capitals',
+                        'id' => '1',
+                        'attributes' => [
+                            'name' => 'Patched Amsterdam'
+                        ]
+                    ]
+                ],
+                'PatchRequest/patched-multi-word-resource',
+            ],
+
         ];
+    }
+
+    /**
+     * @param string $url The endpoint to hit
+     * @param string $expectedResponseFile The file to find the expected jsonapi response in
+     * @return void
+     * @dataProvider patchProvider
+     */
+    public function testPatch($url, $body, $expectedResponseFile)
+    {
+        $this->markTestSkipped("Awaiting PATCH analysis");
 
         $this->configRequest([
             'headers' => [
                 'Accept' => 'application/vnd.api+json',
                 'Content-Type' => 'application/vnd.api+json'
             ],
-            'input' => json_encode($postData)
+            'input' => json_encode($body)
         ]);
 
-        $this->patch('/countries/1');
-
-        $this->assertResponseOk();
-        $this->_assertJsonApiResponseHeaders();
+        # execute the PATCH request
+        $this->patch($url);
         $this->assertResponseCode(200);
-        $this->assertResponseNotEmpty();
+        $this->_assertJsonApiResponseHeaders();
+        $this->assertResponseEquals($this->_getExpected($expectedResponseFile));
     }
 }
