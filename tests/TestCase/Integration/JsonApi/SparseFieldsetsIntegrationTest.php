a0c42993cb27a798dd7e632ee994716a04f22a58
bravo-kernel <bravo-kernel@users.noreply.github.com>
2018-05-18 21:12:33 +0000

    Move to SparseFieldsets

--- tests/TestCase/Integration/JsonApi/SparseFieldsetsIntegrationTest.php
+++ tests/TestCase/Integration/JsonApi/SparseFieldsetsIntegrationTest.php
@@ -17,79 +17,79 @@ class SparseFieldsetsIntegrationTest extends JsonApiBaseTestCase
             // assert "single-field" sparse for index actions
             'single-field sparse index' => [
                 '/countries?fields[countries]=name',
-                'sparse-fieldsets/index_single_field_sparse.json',
+                'index_single_field_sparse.json',
             ],
             'single-field sparse for included index data' => [
                 '/countries?include=currencies&fields[currencies]=id,name',
-                'sparse-fieldsets/index_single_field_sparse_for_included_data.json',
+                'index_single_field_sparse_for_included_data.json',
             ],
             'combined single-field sparse index (both primary and included data)' => [
                 '/countries?fields[countries]=name,currency&include=currencies&fields[currencies]=id,code',
-                'sparse-fieldsets/index_single_field_sparse_for_primary_and_included_data.json'
+                'index_single_field_sparse_for_primary_and_included_data.json'
             ],
 
             // assert "single-field" sparse for view actions
             'single-field sparse view' => [
                 '/countries/1?fields[countries]=name',
-                'sparse-fieldsets/view_single_field_sparse.json',
+                'view_single_field_sparse.json',
             ],
             'single-field sparse for included view data' => [
                 '/countries/1?include=currencies&fields[currencies]=id,name',
-                'sparse-fieldsets/view_single_field_sparse_for_included_data.json',
+                'view_single_field_sparse_for_included_data.json',
             ],
             'combined single-field sparse view (both primary and included data)' => [
                 '/countries/1?fields[countries]=name,currency&include=currencies&fields[currencies]=id,code',
-                'sparse-fieldsets/view_single_field_sparse_for_primary_and_included_data.json'
+                'view_single_field_sparse_for_primary_and_included_data.json'
             ],
 
             // assert "multi-field" sparse for index actions
             'multi-field sparse index' => [
                 '/countries?fields[countries]=name,code',
-                'sparse-fieldsets/index_multi_field_sparse.json',
+                'index_multi_field_sparse.json',
             ],
             'multi-field sparse for included index data' => [
                 '/countries?include=currencies&fields[currencies]=id,name,code',
-                'sparse-fieldsets/index_multi_field_sparse_for_included_data.json'
+                'index_multi_field_sparse_for_included_data.json'
             ],
             'combined multi-field sparse index (both primary and included data)' => [
                 '/countries?fields[countries]=code,name,currency&include=currencies&fields[currencies]=id,code,name',
-                'sparse-fieldsets/index_multi_field_sparse_for_primary_and_included_data.json'
+                'index_multi_field_sparse_for_primary_and_included_data.json'
             ],
 
             // assert "multi-field" sparse for view actions
             'multi-field sparse view' => [
                 '/countries/1?fields[countries]=name,code',
-                'sparse-fieldsets/view_multi_field_sparse.json',
+                'view_multi_field_sparse.json',
             ],
             'multi-field sparse for included view data' => [
                 '/countries/1?include=currencies&fields[currencies]=id,name,code',
-                'sparse-fieldsets/view_multi_field_sparse_for_included_data.json',
+                'view_multi_field_sparse_for_included_data.json',
             ],
             'combined multi-field sparse view (both primary and included data)' => [
                 '/countries/1?fields[countries]=name,code,currency&include=currencies&fields[currencies]=id,code,name',
-                'sparse-fieldsets/view_multi_field_sparse_for_primary_and_included_data.json'
+                'view_multi_field_sparse_for_primary_and_included_data.json'
             ],
 
             'sparse fieldsets - view no relationship' => [
                 '/countries/1?fields[countries]=name',
-                'sparse-fieldsets/view_no_relationship.json',
+                'view_no_relationship.json',
             ],
             'sparse fields - index no relationship' => [
                 '/countries?fields[countries]=name',
-                'sparse-fieldsets/index_no_relationship.json',
+                'index_no_relationship.json',
             ],
             'sparse fields - index with include' => [
                 '/countries?fields[countries]=name,currency&include=currencies&fields[currencies]=id,name',
-                'sparse-fieldsets/index_with_include.json',
+                'index_with_include.json',
             ],
 
             'sparse fieldsets - index with include and sort' => [
                 '/countries?fields[countries]=name,currency&include=currencies&fields[currencies]=id,name&sort=name',
-                'sparse-fieldsets/index_with_include_and_sort.json',
+                'index_with_include_and_sort.json',
             ],
             'sparse fieldsets - index with include and sort desc' => [
                 '/countries?fields[countries]=name,currency&include=currencies&fields[currencies]=id,name&sort=-name',
-                'sparse-fieldsets/index_with_include_and_sort_desc.json',
+                'index_with_include_and_sort_desc.json',
             ],
         ];
     }
@@ -119,7 +119,7 @@ class SparseFieldsetsIntegrationTest extends JsonApiBaseTestCase
 
         $this->assertResponseSuccess();
         $this->_assertJsonApiResponseHeaders();
-        $this->assertResponseEquals($this->_getExpected($expectedFile));
+        $this->assertResponseEquals($this->_getExpected('SparseFieldsets' . DS . $expectedFile));
     }
 
     /**
@@ -135,6 +135,6 @@ class SparseFieldsetsIntegrationTest extends JsonApiBaseTestCase
 
         $this->assertResponseSuccess();
         $this->_assertJsonApiResponseHeaders();
-        $this->assertResponseEquals($this->_getExpected($expectedFile));
+        $this->assertResponseEquals($this->_getExpected('SparseFieldsets' . DS . $expectedFile));
     }
 }
