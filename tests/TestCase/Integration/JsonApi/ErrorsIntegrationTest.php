52477e5cd8120c45546bdff3a531bf56cc47ad7a
bravo-kernel <bravo-kernel@users.noreply.github.com>
2018-05-19 08:26:23 +0000

    Add ErrorsIntegrationTest

---
+++ tests/TestCase/Integration/JsonApi/ErrorsIntegrationTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace CrudJsonApi\Test\TestCase\Integration\JsonApi;
+
+use CrudJsonApi\Test\TestCase\Integration\JsonApiBaseTestCase;
+
+class ErrorsIntegrationTest extends JsonApiBaseTestCase
+{
+    /**
+     * Make sure 404 Not Found is properly handled for Resources
+     */
+    public function test404ForResources() {
+        $this->get('/countries/666');
+        $this->assertResponseCode(404);
+        $this->_assertJsonApiResponseHeaders();
+        $this->assertResponseEquals($this->_getExpected('Errors' . DS . '404-error-not-found-resource.json'));
+    }
+
+
+    /**
+     * Make sure 404 Not Found is properly handled for Resources
+     */
+    public function test404ForCollections() {
+        $this->get('/nonexistents');
+        $this->assertResponseCode(404);
+        $this->_assertJsonApiResponseHeaders();
+        $this->assertResponseEquals($this->_getExpected('Errors' . DS . '404-error-not-found-collection.json'));
+    }
+}
