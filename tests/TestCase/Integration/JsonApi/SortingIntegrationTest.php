d374b5dd872ab46e2152d5a82c1cd115de8775bd
bravo-kernel <bravo-kernel@users.noreply.github.com>
2018-05-19 09:07:14 +0000

    Dasherize Sorting fixtures

--- tests/TestCase/Integration/JsonApi/SortingIntegrationTest.php
+++ tests/TestCase/Integration/JsonApi/SortingIntegrationTest.php
@@ -16,66 +16,66 @@ class SortingIntegrationTest extends JsonApiBaseTestCase
         return [
             'unsorted' => [
                 '/currencies',
-                'get_currencies_no_sort.json',
+                'get-currencies-no-sort.json',
             ],
             'sorted' => [
                 '/currencies?sort=code',
-                'get_currencies_no_sort.json',
+                'get-currencies-no-sort.json',
             ],
             'sorted descending' => [
                 '/currencies?sort=-code',
-                'get_currencies_sort_by_code_desc.json',
+                'get-currencies-sort-by-code-desc.json',
             ],
             'sort single field ascending (default)' => [
                 '/countries?sort=name',
-                'sort_single_field_ascending.json',
+                'sort-single-field-ascending.json',
             ],
             'sort primary data by related resource (single field)' => [
                 '/countries?include=national_capital&sort=national_capital.name&limit=10',
-                'sort_primary_data_by_related_resource_single_field.json',
+                'sort-primary-data-by-related-resource-single-field.json',
             ],
             'unsorted with include' => [
                 '/currencies?include=countries',
-                'get_currencies_and_countries_no_sort.json',
+                'get-currencies-and-countries-no-sort.json',
             ],
             'sorted with include' => [
                 '/countries?include=currency&sort=code',
-                'sorted_with_include.json',
+                'sorted-with-include.json',
             ],
             'sorted with include page 2' => [
                 '/countries?include=currency&sort=code&page=2',
-                'sorted_with_include_page_2.json',
+                'sorted-with-include-page-2.json',
             ],
             'sorted desc with include' => [
                 '/countries?include=currency&sort=-code',
-                'sorted_desc_with_include.json'
+                'sorted-desc-with-include.json'
             ],
             'multi fields sorting' => [
                 '/countries?include=currency&sort=dummy_counter,code',
-                'multi_fields_sorting.json'
+                'multi-fields-sorting.json'
             ],
             'multi fields sorting with direction' => [
                 '/countries?include=currency&sort=-dummy_counter,-code',
                 //'get_currencies_and_countries_sort_by_code_desc.json',
-                'multi_fields_sorting_with_direction.json'
+                'multi-fields-sorting-with-direction.json'
             ],
 
             'hasMany - index with multi fields sorting with direction' => [
                 '/currencies?include=countries&sort=code,-countries.code&limit=5',
-                'has_many_index_with_multi_fields_sorting_with_direction.json',
+                'hasmany-index-with-multi-fields-sorting-with-direction.json',
             ],
 
             'view with multi fields sorting with direction' => [
                 '/currencies/1?include=countries&sort=code,-countries.code',
-                'get_currency_and_countries_sort_by_code_desc.json',
+                'get-currency-and-countries-sort-by-code-desc.json',
             ],
             'sort fields not in include' => [
                 '/countries?include=currencies&sort=code,national_capitals.name',
-                'get_countries_and_currencies_sort_by_code.json',
+                'get-countries-and-currencies-sort-by-code.json',
             ],
             'sort fields in include' => [
                 '/countries?include=currencies,national_capitals&sort=countries.code,national_capitals.name',
-                'get_countries_and_currencies_and_capitals_sort_by_code.json',
+                'get-countries-and-currencies-and-capitals-sort-by-code.json',
             ],
             // Aside from the pagination payload in "meta", this should
             // produce identical result as 'sort fields not in include'.
@@ -84,7 +84,7 @@ class SortingIntegrationTest extends JsonApiBaseTestCase
             // - dot notation against primary type is correctly handled
             'sort primary field in dot notation' => [
                 '/countries?include=currencies&sort=countries.code,national_capitals.name',
-                'sort_primary_field_in_dot_notation.json',
+                'sort-primary-field-in-dot-notation.json',
             ],
         ];
     }
@@ -97,7 +97,7 @@ class SortingIntegrationTest extends JsonApiBaseTestCase
         return [
             'pagination' => [
                 '/national-cities?page=2&limit=2',
-                'national_cities_absolute_links.json',
+                'national-cities-absolute-links.json',
             ],
         ];
     }
