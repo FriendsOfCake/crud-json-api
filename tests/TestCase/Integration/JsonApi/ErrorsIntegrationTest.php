3875c28aaa5ef4565ad63cd2dc2b3e38034baecd
bravo-kernel <bravo-kernel@users.noreply.github.com>
2018-05-19 12:31:45 +0000

    Add response compare to finish the 404 error integration test

--- tests/TestCase/Integration/JsonApi/ErrorsIntegrationTest.php
+++ tests/TestCase/Integration/JsonApi/ErrorsIntegrationTest.php
@@ -1,29 +1,34 @@
 <?php
 namespace CrudJsonApi\Test\TestCase\Integration\JsonApi;
 
+use Cake\Core\Configure;
 use CrudJsonApi\Test\TestCase\Integration\JsonApiBaseTestCase;
 
 class ErrorsIntegrationTest extends JsonApiBaseTestCase
 {
     /**
-     * Make sure 404 Not Found is properly handled for Resources
+     * Make sure 404 Not Found is properly handled for Resources in non-debug mode
      */
-    public function test404ForResources()
+    public function test404ForResourcesInProductionMode()
     {
+        Configure::write('debug', false);
+
         $this->get('/countries/666');
         $this->assertResponseCode(404);
         $this->_assertJsonApiResponseHeaders();
-        #$this->assertResponseEquals($this->_getJsonApiResponseBody('Errors' . DS . '404-error-not-found-resource.json'));
+        $this->assertResponseEquals($this->_getJsonApiResponseBody('Errors' . DS . '404-error-not-found-resource-production-mode.json'));
     }
 
     /**
-     * Make sure 404 Not Found is properly handled for Resources
+     * Make sure 404 Not Found is properly handled for Resources in non-debug mode
      */
-    public function test404ForCollections()
+    public function test404ForCollectionsInProductionMode()
     {
+        Configure::write('debug', false);
+
         $this->get('/nonexistents');
         $this->assertResponseCode(404);
         $this->_assertJsonApiResponseHeaders();
-        #$this->assertResponseEquals($this->_getJsonApiResponseBody('Errors' . DS . '404-error-not-found-collection.json'));
+        $this->assertResponseEquals($this->_getJsonApiResponseBody('Errors' . DS . '404-error-not-found-collection-production-mode.json'));
     }
 }
