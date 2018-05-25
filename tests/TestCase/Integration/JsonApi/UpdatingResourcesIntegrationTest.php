22e1b67a559489adbd15af8afe015d438c8c507b
bravo-kernel <bravo-kernel@users.noreply.github.com>
2018-05-25 17:12:57 +0000

    Add updating a single-word resource with no relationships

--- tests/TestCase/Integration/JsonApi/UpdatingResourcesIntegrationTest.php
+++ tests/TestCase/Integration/JsonApi/UpdatingResourcesIntegrationTest.php
@@ -7,35 +7,31 @@ class UpdatingResourcesIntegrationTest extends JsonApiBaseTestCase
 {
 
     /**
-     * PhpUnit Data Provider for testing (only) successful PATCH requests.
+     * PhpUnit Data Provider that will call `testUpdateResource()` for every array entry
+     * so we can test multiple successful PATCH requests without repeating ourselves.
      *
-     * Each array-entry is executed against the `testPatch()` method in this Integration test which
-     * will automatically check for the correct (jsonapi) response headers, the expected HTTP Response
-     * Code 200 and the expected (jsonapi) response body.
+     * Please note that we MUST update records that already exist in the database fixtures.
      *
      * @return array
      */
-    public function patchProvider()
+    public function updateResourceProvider()
     {
         return [
-            # Test updating a single-word resource.
-            #
-            # By leaving out the `dummy-counter` attribute we also assert the following JSON API criteria:
+            # changing USD to RUB
+            'update-single-word-no-relationships-all-attributes' => [
+                '/currencies/2', // URL
+                'update-currency-no-relationships-all-attributes.json', // Fixtures/JsonApiRequestBodies/UpdatingResources
+                'updated-currency-no-relationships-all-attributes.json' // Fixtures/JsonApiResponseBodies/UpdatingResources
+            ],
+
+            # In this test, by leaving out the `code` attribute, we also assert the JSON API criteria:
             # "If a request does not include all of the attributes for a resource, the server MUST
             #  interpret the missing attributes as if they were included with their current values.
             #  The server MUST NOT interpret missing attributes as null values."
-            #
-            'patch-single-word-resource' => [
-                '/countries/1', // URL
-                'patch-country.json', // Fixtures/JsonApiRequestBodies
-                'patch-country.json' // Fixtures/JsonApiResponseBodies
-            ],
-
-            # Test updating a multi-word resource.
-            'patch-multi-word-resource' => [
-                '/national-capitals/1',
-                'patch-national-capital.json',
-                'patch-national-capital.json'
+            'update-single-word-no-relationships-single-attribute' => [
+                '/currencies/2',
+                'update-currency-no-relationships-single-attribute.json',
+                'updated-currency-no-relationships-single-attribute.json'
             ],
         ];
     }
@@ -45,12 +41,10 @@ class UpdatingResourcesIntegrationTest extends JsonApiBaseTestCase
      * @param string $body JSON API body in CakePHP array format
      * @param string $expectedResponseFile The file to find the expected jsonapi response in
      * @return void
-     * @dataProvider patchProvider
+     * @dataProvider updateResourceProvider
      */
-    public function testPatch($url, $requestBodyFile, $expectedResponseFile)
+    public function testUpdateResource($url, $requestBodyFile, $expectedResponseFile)
     {
-        #$this->markTestSkipped("Awaiting PATCH analysis");
-
         $this->configRequest([
             'headers' => [
                 'Accept' => 'application/vnd.api+json',
@@ -61,8 +55,10 @@ class UpdatingResourcesIntegrationTest extends JsonApiBaseTestCase
 
         # execute the PATCH request
         $this->patch($url);
+
+        # assert response
         $this->assertResponseCode(200);
         $this->_assertJsonApiResponseHeaders();
-        #$this->assertResponseEquals($this->_getExpectedResponseBody('UpdatingResources' . DS . $expectedResponseFile));
+        $this->assertResponseEquals($this->_getExpectedResponseBody('UpdatingResources' . DS . $expectedResponseFile));
     }
 }
