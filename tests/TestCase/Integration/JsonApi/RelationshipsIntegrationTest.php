7d74ae6472c40591d11388a8704c3c8b6420f96e
Walther Lalk <dakota@users.noreply.github.com>
2020-08-13 11:27:56 +0200

    Full integration tests for Relationships

--- tests/TestCase/Integration/JsonApi/RelationshipsIntegrationTest.php
+++ tests/TestCase/Integration/JsonApi/RelationshipsIntegrationTest.php
@@ -93,20 +93,33 @@ class RelationshipsIntegrationTest extends JsonApiBaseTestCase
             ]
         );
 
-        # execute the POST request
+        // execute the POST request
         $this->post($url);
 
-        # assert the response
-        $this->assertResponseCode(200); # https://jsonapi.org/format/#crud-updating-relationship-responses-200
+        // assert the response
+        $this->assertResponseCode(200); // https://jsonapi.org/format///crud-updating-relationship-responses-200
         $this->_assertJsonApiResponseHeaders();
         $this->assertResponseNotEmpty();
         $this->assertResponseSameAsFile('Relationships' . DS . $expectedResponseFile);
     }
 
     /**
-     * @return void
+     * @return \string[][]
      */
-    public function testNoPostOneToOne()
+    public function toOne()
+    {
+        return [
+            'POST' => ['post'],
+            'DELETE' => ['delete']
+        ];
+    }
+
+    /**
+     * @param string $method Method to call
+     * @return void
+     * @dataProvider toOne
+     */
+    public function testNoToOne($method)
     {
         $this->configRequest(
             [
@@ -125,12 +138,174 @@ class RelationshipsIntegrationTest extends JsonApiBaseTestCase
             ]
         );
 
-        # execute the POST request
-        $this->post('/countries/2/relationships/currency');
+        // execute the POST request
+        $this->{$method}('/countries/2/relationships/currency');
 
-        # assert the response
-        $this->assertResponseCode(403); # https://jsonapi.org/format/#crud-updating-relationship-responses-403
+        // assert the response
+        $this->assertResponseCode(403); // https://jsonapi.org/format///crud-updating-relationship-responses-403
         $this->_assertJsonApiResponseHeaders();
         $this->assertResponseNotEmpty();
+    }
+
+    /**
+     * @return \string[][]
+     */
+    public function missingRecordProvider()
+    {
+        return [
+            'POST' => ['post'],
+            'PATCH' => ['patch']
+        ];
+    }
+
+    /**
+     * @return void
+     * @dataProvider missingRecordProvider
+     */
+    public function testMissingRecords($method)
+    {
+        $this->configRequest(
+            [
+                'headers' => [
+                    'Accept' => 'application/vnd.api+json',
+                    'Content-Type' => 'application/vnd.api+json',
+                ],
+                'input' => json_encode(
+                    [
+                        'data' => [
+                            [
+                                'type' => 'cultures',
+                                'id' => 1,
+                            ],
+                            [
+                                'type' => 'cultures',
+                                'id' => 10,
+                            ],
+                        ],
+                    ]
+                ),
+            ]
+        );
+
+        // execute the POST request
+        $this->{$method}('/countries/2/relationships/cultures');
+
+        // assert the response
+        $this->assertResponseCode(404); // http://jsonapi.org/format///crud-updating-responses-404
+        $this->_assertJsonApiResponseHeaders();
+        $this->assertResponseNotEmpty();
+        $this->assertResponseContains('Not all requested records could be found. Missing IDs are 10');
+    }
+
+    /**
+     * @return array
+     */
+    public function patchProvider()
+    {
+        return [
+            'one-to-many: replace culture relationship for country' => [
+                '/countries/2/relationships/cultures',
+                'replace-culture-relationship.json',
+                'patch-replace-culture-relationship.json',
+            ],
+            'one-to-many: clear all culture for country' => [
+                '/countries/2/relationships/cultures',
+                'clear-culture-relationship.json',
+                'patch-clear-culture-relationship.json',
+            ],
+            'many-to-many: replace language to country' => [
+                '/countries/1/relationships/languages',
+                'replace-language-relationship.json',
+                'patch-replace-language-relationship.json',
+            ],
+            'many-to-one: replace currency of country' => [
+                '/countries/1/relationships/currency',
+                'replace-currency-relationship.json',
+                'patch-replace-currency-relationship.json',
+            ],
+        ];
+    }
+
+    /**
+     * @param string $url The endpoint to hit
+     * @param string $requestBodyFile The file to find the request in
+     * @param string $expectedResponseFile The file to find the expected response in
+     * @return void
+     * @dataProvider patchProvider
+     */
+    public function testPatch($url, $requestBodyFile, $expectedResponseFile)
+    {
+        $this->configRequest(
+            [
+                'headers' => [
+                    'Accept' => 'application/vnd.api+json',
+                    'Content-Type' => 'application/vnd.api+json',
+                ],
+                'input' => $this->_getJsonApiRequestBody('Relationships' . DS . $requestBodyFile),
+            ]
+        );
+
+        $this->disableErrorHandlerMiddleware();
+        // execute the POST request
+        $this->patch($url);
+
+        // assert the response
+        $this->assertResponseCode(200); // https://jsonapi.org/format///crud-updating-relationship-responses-200
+        $this->_assertJsonApiResponseHeaders();
+        $this->assertResponseNotEmpty();
+        $this->assertResponseSameAsFile('Relationships' . DS . $expectedResponseFile);
+    }
+
+    /**
+     * @return array
+     */
+    public function deleteProvider()
+    {
+        return [
+            'one-to-many: delete culture relationship for country' => [
+                '/countries/2/relationships/cultures',
+                'delete-culture-relationship.json',
+                'delete-culture-relationship.json',
+            ],
+            'one-to-many: delete culture relationship that does not exist for country' => [
+                '/countries/2/relationships/cultures',
+                'delete-not-existing-culture-relationship.json',
+                'delete-not-existing-culture-relationship.json',
+            ],
+            'many-to-many: delete language to country' => [
+                '/countries/1/relationships/languages',
+                'delete-language-relationship.json',
+                'delete-language-relationship.json',
+            ],
+        ];
+    }
+
+    /**
+     * @param string $url The endpoint to hit
+     * @param string $requestBodyFile The file to find the request in
+     * @param string $expectedResponseFile The file to find the expected response in
+     * @return void
+     * @dataProvider deleteProvider
+     */
+    public function testDelete($url, $requestBodyFile, $expectedResponseFile)
+    {
+        $this->configRequest(
+            [
+                'headers' => [
+                    'Accept' => 'application/vnd.api+json',
+                    'Content-Type' => 'application/vnd.api+json',
+                ],
+                'input' => $this->_getJsonApiRequestBody('Relationships' . DS . $requestBodyFile),
+            ]
+        );
+
+        // execute the DELETE request
+        $this->delete($url);
+
+        // assert the response
+        $this->assertResponseCode(200); // https://jsonapi.org/format///crud-updating-relationship-responses-200
+        $this->_assertJsonApiResponseHeaders();
+        $this->assertResponseNotEmpty();
+        $this->assertResponseSameAsFile('Relationships' . DS . $expectedResponseFile);
     }
 }
